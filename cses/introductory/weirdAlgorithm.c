e8e3cddcd48d007e98934d7951eb375a3cabf26a
Mahtab-Abdullah <abdullahahad397@gmail.com>
2026-07-04 19:40:33 +0600

    Solve CSES 1068 - Weird Algorithm

---
+++ cses/introductory/weirdAlgorithm.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void algo(long long n) {
+  while (n != 1) {
+    printf("%lld ", n);
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else {
+      n = (3 * n + 1);
+    }
+  }
+  printf("1\n");
+}
+
+int main() {
+  long long n;
+  if (scanf("%lld", &n) == 1) {
+    algo(n);
+  } else {
+    return 0;
+  }
+}
