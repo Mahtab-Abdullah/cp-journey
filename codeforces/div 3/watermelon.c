6004c4bfd43a69232ef9f7f09677ec540d3768a4
Mahtab-Abdullah <abdullahahad397@gmail.com>
2026-09-21 23:36:20 +0600

    started codeforces

---
+++ codeforces/div 3/watermelon.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  int w;
+
+  if (scanf("%d", &w) != 1) {
+    return -1;
+  }
+
+  if (((w - 2) % 2 == 0) && (w - 2 > 0)) {
+    printf("YES\n");
+  } else {
+    printf("NO\n");
+  }
+  return 0;
+}
